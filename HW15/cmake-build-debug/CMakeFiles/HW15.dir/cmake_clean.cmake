620c4a919ac0d970be8228ce493e1e833186cf70
Qi-Rui Hong <y6602016@gmail.com>
2021-04-24 17:18:43 +0800

    Add HW15

---
+++ HW15/cmake-build-debug/CMakeFiles/HW15.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW15.dir/main.cpp.o"
+  "HW15"
+  "HW15.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
